8c60d13fdc8411f3105c1a53e1bb1420bbf57437
Ashish <ashishdargude1432@gmail.com>
2022-10-11 05:34:57 +0530

    deleting not required

--- src/test2/Test1.java
+++
@@ -1,7 +0,0 @@
-package test2;
-
-public class Test1 {
-public static void main(String[] args) {
-	System.out.println("create new file");
-}
-}
--- src/test2/trial.java
+++
@@ -1,8 +0,0 @@
-package test2;
-
-public class trial {
-	public static void main(String[] args) {
-		System.out.println("Hello Team ! Welcome to Java Sessions");
-	}
-}
-//public static void main(String[] args) {
